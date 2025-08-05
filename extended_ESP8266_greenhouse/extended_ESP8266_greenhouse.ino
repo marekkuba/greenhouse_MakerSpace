b9f77406597b7122969598699a6b988a49dde520
Sconguser <scongi.30@gmail.com>
2025-08-05 16:43:21 +0200

    added network config

--- extended_ESP8266_greenhouse/extended_ESP8266_greenhouse.ino
+++ extended_ESP8266_greenhouse/extended_ESP8266_greenhouse.ino
@@ -8,11 +8,13 @@
 #include <map>       // Dodane dla std::map
 
 // --- Wi‑Fi / MQTT config --------------------------------------------
-#define WIFI_SSID      "SSID"
-#define WIFI_PASSWORD  "PASSWORD"
-#define MQTT_HOST      IPAddress(192,168,1,XXX) //ustawic poprawne IP MQTT brokera
-#define MQTT_PORT      1883
-
+struct NetworkConfig {
+  String wifi_ssid;
+  String wifi_password;
+  IPAddress mqtt_host;
+  uint16_t mqtt_port;
+};
+NetworkConfig netConfig;
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 AsyncMqttClient mqttClient;
@@ -59,7 +61,7 @@ void onMqttMessage(char* topic, char* payload,
                    size_t len, size_t idx, size_t total);
 String makeTopic(const DeviceConfig& dev);
 void loadConfig();
-
+void loadNetworkConfig();
 String makeTopic(const DeviceConfig& dev) {
   String t = "greenhouse";
   if (dev.flowerzoneId) t += "/zone" + String(dev.flowerzoneId);
@@ -143,6 +145,52 @@ void loadConfig() {
     devices.push_back(dev);
   }
   Serial.printf("[CONFIG] Loaded %d devices\n", (int)devices.size());
+}
+
+
+void loadNetworkConfig() {
+  if (!LittleFS.begin()) {
+    Serial.println("[ERROR] LittleFS mount failed\n");
+    return;
+  }
+  
+  auto filename = "/network_config.json";
+
+  if (!LittleFS.exists(filename)) {
+    Serial.println("[ERROR] %s not found\n", filename);
+    return;
+  }
+  
+  File f = LittleFS.open(filename, "r");
+  if (!f) {
+    Serial.println("[ERROR] Failed to open %s\n", filename);
+    return;
+  }
+  
+  size_t size = f.size();
+  char* buf = new char[size];
+  f.readBytes(buf, size);
+  f.close();
+
+  StaticJsonDocument<2048> doc;
+  auto err = deserializeJson(doc, buf, size);
+  delete[] buf;  // Zwolnienie pamięci
+  
+  if (err) {
+    Serial.printf("[ERROR] Config parse failed: %s\n", err.c_str());
+    return;
+  }
+
+  JsonArray arr = doc.as<JsonArray>();
+  netConfig.wifi_ssid = doc["wifi_ssid"].as<String>();
+  netConfig.wifi_password = doc["wifi_password"].as<String>();
+  String mqttHostStr = doc["mqtt_host"].as<String>();
+  netConfig.mqtt_port = doc["mqtt_port"] | 1883; // default 1883
+
+  int parts[4] = {0};
+  sscanf(mqttHostStr.c_str(), "%d.%d.%d.%d", &parts[0], &parts[1], &parts[2], &parts[3]);
+  netConfig.mqtt_host = IPAddress(parts[0], parts[1], parts[2], parts[3]);
+  Serial.printf("[CONFIG] Loaded connection config\n");
 }
 
 void setupWifi() {
@@ -355,7 +403,7 @@ void setup() {
   }
 
   loadConfig();
-
+  loadNetworkConfig();
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
 
